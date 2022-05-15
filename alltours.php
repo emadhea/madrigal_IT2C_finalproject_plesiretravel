f8120e592badee4420952677caed543bf945c7fc
MadrigalEmaDhea <emamadrigal64@gmail.com>
2022-05-15 08:57:27 +0800

    second commit

---
+++ alltours.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8">
+    <title>Plesire | Travel</title>
+    <meta content="" name="description">
+    <meta content="" name="author">
+    <meta content="" name="keywords">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <!-- favicon -->
+    <link href="img/favicon.gif" rel="icon" sizes="32x32" type="image/png">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- font themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- font awesome CSS -->
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+    <!-- date picker CSS -->
+    <link href="css/datepicker.min.css" rel="stylesheet">
+    <!-- revolution slider css -->
+    <link rel="stylesheet" type="text/css" href="css/fullwidth.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+    <link rel="stylesheet" href="css/rev-settings.css" type="text/css">
+    <!-- on3step CSS -->
+    <link href="css/animated-on3step.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <link href="css/on3step-style.css" rel="stylesheet">
+    <link href="css/queries-on3step.css" media="all" rel="stylesheet">
+  </head>
+  
+  <body>
+  
+    <!-- preloader -->
+    <div class="bg-preloader-white"></div>
+    <div class="preloader-white">
+      <div class="mainpreloader">
+        <span></span>
+      </div>
+    </div>
+    <!-- preloader end -->
+    
+   <!-- content wraper -->
+   <div class="content-wrapper">
+    
+   <header class="init">
+    
+    <!-- subnav -->
+    <div class="container-fluid m-5-hor">
+    <div class="row">
+        <div class="subnav">
+
+         <div class="col-md-6">
+          <div class="left">
+            <div class="social-icons-subnav hidden-sm hidden-xs">
+                <div>Call Us : +63927 6073 941</div>
+            </div>
+          </div>
+         </div>
+         
+         <div class="col-md-6">
+          <div class="right">
+            <div id="sub-icon" class="social-icons-subnav">
+                <a href="#"><span class="ti-facebook"></span></a>
+                <a href="#"><span class="ti-dribbble"></span></a>
+                <a href="#"><span class="ti-twitter"></span></a>
+                <a href="#"><span class="ti-instagram"></span></a>
+                <a href="#"><span class="ti-linkedin"></span></a>
+            </div>
+            <div class="social-icons-subnav">
+              <a data-toggle="modal" data-target="#fLogin"><span class="ti-lock"></span> Login</a>
+            </div>    
+            <div class="social-icons-subnav hidden-sm hidden-xs">
+              <a data-toggle="modal" data-target="#fsignUp"><span class="ti-user"></span> Sign up</a>
+            </div>
+          </div>
+         </div>
+        
+        </div>
+    </div>
+    </div>
+    <!-- subnav end -->
+    
+      <!-- nav -->
+      <div class="navbar-default-white navbar-fixed-top">
+        <div class="container-fluid m-5-hor">
+        <div class="row">   
+        
+           <!-- menu mobile display -->
+           <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse">
+           <span class="icon icon-bar"></span> 
+           <span class="icon icon-bar"></span> 
+           <span class="icon icon-bar"></span></button> 
+          
+          <!-- logo --> 
+          <a class="navbar-brand white" href="index.php">
+          <img class="white" alt="logo" src="img/logo.png">
+          </a>
+          <!-- logo end -->
+          
+          <!-- mainmenu start -->
+          <div class="white menu-init" id="main-menu">
+            <nav id="menu-center">
+              <ul>
+                <li><a href="#">Gallery <i class="fa fa-angle-down"></i></a> <ul>
+                    <li><a href="alltours.php">All Tours</a></li>
+                  </ul>
+                </li>
+                <li><a  href="#">Destination <i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="island.php">Island</a></li>
+                    <li><a href="falls.php">Falls</a></li>
+                    <li><a href="cave.php">Cave</a></li>
+                    <li><a href="beach.php">Beach</a></li>
+                    <li><a href="cathedral.php">Cathedral</a></li>
+                    <li><a href="mountain.php">Mountain</a></li>
+                    <li><a href="resort.php">Resort</a></li>
+                    </ul>
+                </li>
+                <li><a  href="#">Blog <i class="fa fa-angle-down"></i></a>
+                  <ul>
+                    <li><a href="blog.php">Blog Post</a></li>
+                  </ul>
+                </li>
+                <li><a  href="contact.php">Contact</a></li>
+                <li><a href="#">About <i class="fa fa-angle-down"></i></a>
+                <ul>
+                    <li><a href="about.php">About Us</a></li>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <!-- mainmenu end -->
+          
+        </div>
+        </div>
+        <!-- container -->
+      </div>
+      <!-- nav end -->
+      </header>
+      
+      <!-- subheader -->
+      <section id="subheader">
+        <div class="container-fluid m-5-hor">
+          <div class="row">
+            <div class="col-md-12">
+              <h1 class="big-heading">
+                Our Gallery
+              </h1>
+              <p>See different spots in Marinduque</p>
+            </div>
+        </div>
+       </div>
+      </section>
+      <!-- subheader end -->
+      
+      <!-- section projects -->
+      <section aria-label="works" id="projects">
+            <div class="container-fluid">
+             <div class="row">
+          
+          <!-- project -->
+          <div class="v-align">
+          <div class="col-md-11 col-xs-12">
+            <!-- filter project -->
+                <ul id="filter-hide" class="init">
+                  <li class="filtter-icon"><i class="ti-layout-grid3-alt"></i>
+                  </li>
+                </ul>
+                
+                <ul id="filter-porto">
+                  <li class="filt-projects-w selected" data-project=".alltours">All Tours
+                  </li>
+                  
+                  <li class="filt-projects-w" data-project=".boac">Boac
+                  </li>
+
+                  <li class="filt-projects-w" data-project=".aus">Buenavista
+                  </li>
+
+                  <li class="filt-projects-w" data-project=".eur">Gasan
+                  </li>
+
+                  <li class="filt-projects-w" data-project=".afr">Mogpog
+                  </li>
+
+                </ul>
+                
+            <!-- filter project end -->
+            
+                <div id="w-gallery-container" class="w-gallery-container">
+                
+                    <div class="w-gallery .alltours">
+                     <div class="projects-grid">
+                      <div class="hovereffect-color">
+                      <img src="img/all-tours/1.jpeg" alt="" class="w-gallery-image">
+                      <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery .alltours">
+                    <div class="projects-grid">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/2.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery .alltours">
+                    <div class="projects-grid">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/3.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery .alltours">
+                    <div class="projects-grid onStep" data-animation="fadeInUp" data-time="0">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/4.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery .alltours">
+                    <div class="projects-grid onStep" data-animation="fadeInUp" data-time="0">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/5.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery .alltours">
+                    <div class="projects-grid onStep" data-animation="fadeInUp" data-time="0">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/6.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                    </div>
+                    
+                    <div class="w-gallery .alltours">
+                     <div class="projects-grid onStep" data-animation="fadeInUp" data-time="0">
+                      <div class="hovereffect-color">
+                      <img src="img/all-tours/7.jpeg" alt="" class="w-gallery-image">
+                      <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery .alltours">
+                    <div class="projects-grid onStep" data-animation="fadeInUp" data-time="0">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/8.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                    
+                    <div class="w-gallery asia">
+                    <div class="projects-grid onStep" data-animation="fadeInUp" data-time="0">
+                    <div class="hovereffect-color">
+                    <img src="img/all-tours/9.jpeg" alt="" class="w-gallery-image">
+                    <div class="overlay">
+                      <div class="v-align wrap">
+                      </div>
+                      </div>
+                      </div>
+                     </div>
+                  </div>
+                  
+                </div>
+                
+                </div>
+                </div>
+                <!-- project end -->
+                
+                </div>
+            </div>
+        </section>
+      <!-- section projects end -->
+      
+      
+      <section class="no-top">
+        <div class="container-fluid m-5-hor">
+          <div class="row goldpage">
+          
+             <div class="col-lg-9 col-md-12 text-center">
+                  <h3>Looking for a quality and unforgettable Travel Destination for your next vacation?</h3>
+              </div>
+
+              <div class="col-lg-2 col-md-12">
+                <div class="btn-content" >
+                             <span class="shine"></span>
+                             <a href="#">Let us know</a>
+                </div>
+              </div>
+
+          </div>
+         </div>
+      </section>
+
+       
+      <!-- footer -->
+      <footer class="main text-center">
+        <div class="container-fluid m-5-hor">
+          <div class="row">
+          
+          <div class="onStep" data-animation="fadeInUp" data-time="300">
+          <div class="col-md-4 text-left">
+          <span><a href="#">Email: companyname@gmail.com</a></span>
+          <span>Copyright - 2018 Plesire Travel All Right Reserved</span>
+          </div>
+          
+          <div class="col-md-4">
+                <span class="logo">
+                <img alt="logo" src="img/logo-white.png">
+                </span>
+            </div>
+            
+            <div class="col-md-4 text-right">
+              <span>129 Park street, New York City, NY 10903</span>
+              <span>Phone: (+6221) 000 888 999</span>
+            </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </footer>
+      <!-- footer end -->
+      
+      
+      <!-- ScrolltoTop -->
+      <div id="totop" class="init">
+        <span class="ti-angle-up"></span>
+      </div>  
+
+              <!-- modal login -->
+              <div id="fLogin" class="modal fade">
+                <div class="modal-dialog modal-login">
+                  <div class="modal-content">
+                    <div class="modal-header">      
+                      <h4 class="modal-title">Member Log In</h4>
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                      <form action="#" method="post">
+                        <div class="form-group">
+                          <label>User Name</label>
+                          <input type="text" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                          <label>Password</label>
+                          <input type="password" class="form-control" required="required">         
+                        </div>
+                        <div class="form-group">
+                          <input type="submit" class="btn btn-primary btn-block btn-lg" value="Log In">
+                        </div>
+                      </form>       
+                      
+                    </div>
+                    <div class="modal-footer">
+                      <a href="#">or Sign Up</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- modal login end -->
+
+              <!-- modal registration -->
+              <div id="fsignUp" class="modal fade">
+                <div class="modal-dialog modal-login">
+                  <div class="modal-content">
+                    <div class="modal-header">      
+                      <h4 class="modal-title">Member Registration</h4>
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <div class="form-group">
+                        <label>First Name</label>
+                        <input type="text" name="firstname" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                        <label>Last Name</label>
+                        <input type="text" name="lastname" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                        <label>Email Address</label>
+                        <input type="email" name="email" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                        <label><input type="checkbox" name="terms"> I agree with the <a href="#">Terms and Conditions</a>.</label>
+                        </div>
+                        <div class="form-group"><input type="submit" value="Sign up" class="btn btn-primary btn-block btn-lg"></div>
+                        <div class="clearfix"></div>
+                      </form>       
+                      
+                    </div>
+                    <div class="modal-footer">
+                      <a href="#">or Log In</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- modal registration end -->      
+
+     
+    </div>
+    <!-- content wraper end --> 
+    
+    <!-- plugin JS -->
+    <script src="plugin/pluginson3step.js"></script> 
+    <script src="plugin/bootstrap.min.js"></script>
+    <script src='plugin/bootstrap-datepicker.min.js'></script>
+    <script src="plugin/sticky.js"></script> 
+    <!-- slider revolution  -->
+    <script src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <!-- on3step JS -->
+    <script src="js/on3step.js"></script>
+    <script src="js/plugin-set.js"></script>
+  </body>
+</html>
